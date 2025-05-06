3d4d378d43dd5cc27bd826dabf699fcb60cc96e2
Amer-Morsy <amermousa341@gmail.com>
2025-05-07 02:51:50 +0300

    39_Show_Images_Rays

--- app/Models/Ray.php
+++ app/Models/Ray.php
@@ -15,8 +15,19 @@ class Ray extends Model
         return $this->belongsTo(Doctor::class,'doctor_id');
     }
 
+    public function employee()
+    {
+        return $this->belongsTo(RayEmployee::class,'employee_id')
+            ->withDefault(['name'=>'noEmployee']);
+    }
+
     public function Patient()
     {
         return $this->belongsTo(Patient::class,'patient_id');
+    }
+
+    public function images()
+    {
+        return $this->morphMany(Image::class, 'imageable');
     }
 }
